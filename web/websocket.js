73ef9d1144cf535ae18331a790705d3e4e8e6ca7
mateo.norena <mateo.norena@localhost>
2015-04-29 11:56:44 -0500

    Corregidos errores, no aparecen eventos de red

--- web/websocket.js
+++ web/websocket.js
@@ -8,36 +8,36 @@ var wsUri = "ws://" + document.location.host + document.location.pathname + "ech
 var websocket = new WebSocket(wsUri);
 var username;
 
-websocket.onOpen=function(evt)(onOpen(evt));
-websocket.onMessage=function(evt)(onMessage(evt));
-websocket.onError=function(evt)(onError(evt));
+websocket.onopen=function(evt){onOpen(evt)};
+websocket.onmessage=function(evt){onMessage(evt)};
+websocket.onerror=function(evt){onError(evt)};
 
 var output = document.getElementById("output");
 
 function join() {
     username = textField.value;
-    websocket.send(username + "enlazado");
+    websocket.send(username + " enlazado");
 }
 
 function send_message() {
     websocket.send(username + ":" + textField.value);
 }
 
-function onOpen() {
+function onOpen(evt) {
     writeToScreen("Conectado a " + wsUri);
 }
 
-function onMessage() {
+function onMessage(evt) {
     console.log("onMessage");
     writeToScreen("Recibido:" + evt.data);
     if (evt.data.indexOf("enlazado") !== -1) {
         userField.innerHTML += evt.data.substring(0, evt.data.indexOf("enlazado")) + "\n";
     } else {
-        chatlogField.innerHTML += evt.data + "\n";
+        chatLogField.innerHTML += evt.data + "\n";
     }
 }
 
-function OnError(evt){
+function onError(evt){
     writeToScreen('<span style="color:red;">ERROR:</span>' + evt.data);
 }
 
